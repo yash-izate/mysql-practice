d6bb0bb2477111361b819f7077ee7dd1c58b833c
Yash Izate <yashizate.softech@gmail.com>
2026-09-08 21:53:11 +0530

    Initial commit

---
+++ query1.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE startersql;
+
+USE startersql;
+
+CREATE TABLE users (
+ id INT AUTO_INCREMENT PRIMARY KEY,
+ name VARCHAR(100) NOT NULL,
+ email VARCHAR(100) UNIQUE NOT NULL,
+ gender ENUM('Male', 'Female', 'Other'),
+ date_of_birth DATE,
+ created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+SELECT * FROM users;
