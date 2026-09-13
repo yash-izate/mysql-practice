c3c2cdf9a28b6bb16bd6a53c73a65a891215ed9e
Yash Izate <yashizate.softech@gmail.com>
2026-09-13 18:56:22 +0530

    joins

--- query4.sql
+++ query4.sql
@@ -439,11 +439,15 @@
 -- UNION
 -- SELECT id, name FROM admin_users;
 
-SELECT name,
-'User' AS role
-FROM users
-UNION
-SELECT name,
-'Admin' AS role
-FROM admin_users;
+-- SELECT name,
+-- 'User' AS role
+-- FROM users
+-- UNION
+-- SELECT name,
+-- 'Admin' AS role
+-- FROM admin_users;
 
+-- SELECT name FROM users
+-- UNION
+-- SELECT name FROM admin_users
+-- ORDER BY name;
